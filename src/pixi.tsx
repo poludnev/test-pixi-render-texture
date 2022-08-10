bb90498901b5565937ac4dd13d36ae5c52abc724
poludnev <poludnev.roman@gmail.com>
2022-08-10 15:11:13 +0300

    Added pixi container

--- src/pixi.tsx
+++ src/pixi.tsx
@@ -1,3 +1,33 @@
+import * as PIXI from 'pixi.js';
+import { useEffect, useRef } from 'react';
+
+const app = new PIXI.Application({
+  width: 700,
+  height: 700,
+  backgroundColor: 0xffcccc,
+});
+
+const sprite1 = new PIXI.Sprite(PIXI.Texture.from('bc-sq-200.png'));
+sprite1.x = 500;
+app.stage.addChild(sprite1);
+
+const sprite2 = new PIXI.Sprite(PIXI.Texture.from('wt-sq-200.png'));
+app.stage.addChild(sprite2);
+
+const renderer = PIXI.autoDetectRenderer();
+const renderTexture = PIXI.RenderTexture.create({ width: 700, height: 700 });
+renderer.render(sprite2, { renderTexture });
+
+const mainSprite = PIXI.Sprite.from(renderTexture);
+app.stage.addChild(mainSprite);
+
 export const PixiContainer = () => {
-  return <div>Pixi Container</div>;
+  const appRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (appRef.current) {
+      appRef.current.appendChild(app.view);
+    }
+  }, []);
+  return <div ref={appRef}></div>;
 };
